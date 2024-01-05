d0b460da5aa276465e37eeb2b654c1c37ef6ad24
seri <battaua58@gmail.com>
2024-01-06 05:17:01 +0900

    動画をwebmにコンバートしながら送信(作成中)

---
+++ tagVideoManager/video/VideoConberter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tagVideoManager
+{
+	internal class VideoConberter
+	{
+		private const int BufferSize = 4096;
+
+		// ffmpegを使ってwebmに変換したデータをメモリに書き込み
+		public static MemoryStream WriteMemory(string path, int startSecond, int timeSpanSecond)
+		{
+			var sb = new StringBuilder();
+			sb.Append($" -ss {startSecond} -t {timeSpanSecond}");
+			sb.Append($" -i {path}");
+			sb.Append($" -pix_fmt yuv420p -map_metadata -1");
+			sb.Append($" -c:v libvpx -crf 30 -b:v 600k -qmin 3 -qmax 40 -colorspace 6 -color_range 0 -color_primaries 6 -color_trc 6");
+			sb.Append($" -an");
+			sb.Append($" -loglevel warning");
+			sb.Append($" -f webm -");	// 標準出力へ
+
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = ".\\ffmpeg\\ffmpeg",
+					Arguments = sb.ToString(),
+					UseShellExecute = false,
+					CreateNoWindow = true,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true
+				},
+				EnableRaisingEvents = true
+			};
+
+			process.Start();
+
+			var ms = new MemoryStream();
+			var ws = new BinaryWriter(ms);
+
+			var rs = new BinaryReader(process.StandardOutput.BaseStream);
+
+			var buffer = rs.ReadBytes(BufferSize);
+			while (buffer.Length == BufferSize)
+			{
+				ws.Write(buffer);
+				buffer = rs.ReadBytes(BufferSize);
+			}
+			ws.Write(buffer);
+			var error = process.StandardError.ReadToEnd();
+			process.WaitForExit();
+			process.Dispose();
+
+			if (error.Contains("Output file is empty"))
+				return null;
+
+			ms.Position = 0;
+			return ms;
+		}
+
+
+
+
+	}
+}
---
+++ tagVideoManager/video/VideoController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security.Policy;
+using System.Web;
+using System.Windows;
+using System.Windows.Forms;
+using Microsoft.Win32.SafeHandles;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;       // Bitmap変換に必要
+using Shell32;
+using tagVideoManager;
+
+namespace videoEditor
+{
+	internal class VideoController : IDisposable
+	{
+		private VideoCapture _vcap = null;
+		// 最後に表示したイメージ
+		private Mat _lastMat = new Mat();
+
+		private string _lastSymbolicLink = string.Empty;
+
+
+		// 現在のフレーム位置取得
+		public int PosFrames { get { return _vcap?.PosFrames ?? 0; } }
+
+		// トータルフレーム取得
+		public int FrameCount { get { return _vcap?.FrameCount ?? 0; } }
+
+
+		// トータル再生時間(1.0f = 1秒)
+		public double TotalTime
+		{
+			get
+			{
+				if (_vcap == null)
+					return 0.0;
+
+				// Fpsが正しく取れない事がある模様。
+				// 
+				return (double)_vcap.FrameCount / _vcap.Fps;
+			}
+		}
+
+
+        // 動画再生開始
+        public VideoController(string path)
+        {
+			_lastSymbolicLink = Path.GetRandomFileName();
+#if true
+			FileIdHelper.CreateFileLink(_lastSymbolicLink, path);
+#else
+			File.CreateSymbolicLink(_lastSymbolicLink, path);
+#endif
+			_vcap = VideoCapture.FromFile(_lastSymbolicLink);
+
+
+			// var mdt = GetTime(_lastSymbolicLink);
+		}
+
+		// 動画の時間を取得
+		// メインスレッドでないと動かない模様
+		public static TimeSpan GetTime(string path)
+		{
+			if (!File.Exists(path))
+				return TimeSpan.Zero;
+
+			var fi = new FileInfo(path);
+			var shell = new Shell();
+			var folder = shell.NameSpace(fi.DirectoryName);
+			var item = folder.ParseName(fi.Name);
+			var data = folder.GetDetailsOf(item, 27);
+			return TimeSpan.Parse(data);
+		}
+
+
+		// 任意のフレームの画像を取得
+		// 指定しない場合は最後設定したフレームの次のフレーム
+        public Bitmap GetImage(int flame = -1)
+        {
+            if (_vcap == null)
+                return null;
+
+            if (!_vcap.IsOpened())
+                return null;
+
+            if (flame >= 0)
+			{
+                _vcap.PosFrames = flame;
+            }
+
+            if (!_vcap.Read(_lastMat))
+                return null;
+
+            if (!_lastMat.IsContinuous())
+                return null;
+
+            return BitmapConverter.ToBitmap(_lastMat);
+        }
+
+
+		// 1.0 = 1秒とする時間指定で任意の時間の画像を取得
+		public Bitmap GetImage(float timeSpan)
+		{
+			if (_vcap == null)
+				return null;
+
+			if (!_vcap.IsOpened())
+				return null;
+
+			// 1秒入れてPosMsecが想定通りか確認
+			_vcap.PosMsec = 1000;
+			if (!_vcap.Read(_lastMat))
+				return null;
+
+
+			if (timeSpan >= 0)
+			{
+				_vcap.PosMsec = (int)(timeSpan * 1000.0f);
+			}
+
+			if (!_vcap.Read(_lastMat))
+				return null;
+
+			if (!_lastMat.IsContinuous())
+				return null;
+
+			return BitmapConverter.ToBitmap(_lastMat);
+		}
+
+#if false
+		private int RecordAppControl()
+		{
+			VideoCapture capture = new VideoCapture();
+			capture.Open(1);
+			if (!capture.IsOpened())
+			{
+				Console.WriteLine("Camera not activated.");
+				return -1;
+			}
+
+			//１(mp4)
+			VideoWriter video = new VideoWriter("result.mp4", OpenCvSharp.FourCC.H264, 30, new OpenCvSharp.Size(600, 300));
+
+			//２(avi)
+			//VideoWriter video = new VideoWriter("result.avi", OpenCvSharp.FourCC.DIVX, 30, new OpenCvSharp.Size(600, 300));
+
+			//３(wmv)
+			//VideoWriter video = new VideoWriter("result.wmv", OpenCvSharp.FourCC.WMV1, 30, new OpenCvSharp.Size(600, 300));
+
+			Mat mat = new Mat();
+			int count = 0;
+
+			System.Timers.Timer recordControlTimer = new System.Timers.Timer(33);
+			recordControlTimer.Elapsed += (sender, e) =>
+			{
+				if (count >= 150)
+				{
+					recordControlTimer.Stop();
+					Console.WriteLine("Timer Stopped\n");
+					video.Dispose();
+					capture.Release();
+				}
+				else
+				{
+					capture.Read(mat);
+					video.Write(mat);
+					pictureBox1.Image = BitmapConverter.ToBitmap(mat);
+					count++;
+				}
+			};
+			recordControlTimer.Start();
+
+			return 0;
+		}
+#endif
+
+		public void Dispose()
+		{
+			_vcap?.Dispose();//Memory release
+			_vcap = null;
+
+			if (!string.IsNullOrEmpty(_lastSymbolicLink))
+			{
+				if (File.Exists(_lastSymbolicLink))
+				{
+					File.Delete(_lastSymbolicLink);
+				}
+
+				_lastSymbolicLink = string.Empty;
+			}
+
+			_lastMat?.Dispose();
+			_lastMat = null;
+		}
+	}
+}
